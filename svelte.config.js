9c4b059a79c949da94ef80cf5eaa90c78b54ef73
JohnHerron <herroncjohn@gmail.com>
2024-02-05 17:22:46 -0800

    Add basic svelte extension framework

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from "sveltekit-adapter-chrome-extension";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+    appDir: "app",
+  },
+};
+
+export default config;
